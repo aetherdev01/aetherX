d7f703b3ea0f64a064ee3863a477f87935bdc2a0
aetherdev01 <aldikhoirudin2@gmail.com>
2026-07-17 02:34:39 +0700

    update

--- app/src/main/java/com/aether/x/core/notification/AdbPairingNotifier.kt
+++ app/src/main/java/com/aether/x/core/notification/AdbPairingNotifier.kt
@@ -4,13 +4,13 @@ import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
-import android.app.RemoteInput
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.os.Build
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
+import androidx.core.app.RemoteInput
 import com.aether.x.MainActivity
 import com.aether.x.R
 import com.aether.x.core.permission.PrivilegeManager
