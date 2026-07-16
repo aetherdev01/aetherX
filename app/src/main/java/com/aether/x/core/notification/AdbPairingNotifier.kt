c020062e6937f20739873237b0595e7da1c550b7
aetherdev01 <aldikhoirudin2@gmail.com>
2026-07-17 02:29:28 +0700

    update

--- app/src/main/java/com/aether/x/core/notification/AdbPairingNotifier.kt
+++ app/src/main/java/com/aether/x/core/notification/AdbPairingNotifier.kt
@@ -111,14 +111,14 @@ object AdbPairingNotifier {
         )
 
         return NotificationCompat.Action.Builder(
-            R.drawable.ic_aetherx_mark,
+            R.drawable.logo,
             context.getString(R.string.adb_pairing_notif_reply_action),
             replyIntent,
         ).addRemoteInput(remoteInput).build()
     }
 
     private fun cancelAction(context: Context): NotificationCompat.Action = NotificationCompat.Action.Builder(
-        R.drawable.ic_aetherx_mark,
+        R.drawable.logo,
         context.getString(R.string.adb_pairing_notif_cancel_action),
         cancelPendingIntent(context),
     ).build()
@@ -134,7 +134,7 @@ object AdbPairingNotifier {
             PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT,
         )
         return NotificationCompat.Builder(context, CHANNEL_ID)
-            .setSmallIcon(R.drawable.ic_aetherx_mark)
+            .setSmallIcon(R.drawable.logo)
             .setCategory(NotificationCompat.CATEGORY_EVENT)
             .setPriority(NotificationCompat.PRIORITY_HIGH)
             .setContentIntent(contentIntent)
