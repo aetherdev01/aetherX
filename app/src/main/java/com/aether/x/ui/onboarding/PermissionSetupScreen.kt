4ba150fc6a56bcf96fa31dcf6913c14896bf6d45
aetherdev01 <aldikhoirudin2@gmail.com>
2026-08-22 23:54:23 +0700

    update

--- app/src/main/java/com/aether/x/ui/onboarding/PermissionSetupScreen.kt
+++ app/src/main/java/com/aether/x/ui/onboarding/PermissionSetupScreen.kt
@@ -27,6 +27,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Circle
+import androidx.compose.material.icons.outlined.BatteryAlert
 import androidx.compose.material.icons.outlined.Bolt
 import androidx.compose.material.icons.outlined.Circle
 import androidx.compose.material.icons.outlined.Layers
@@ -44,6 +45,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
